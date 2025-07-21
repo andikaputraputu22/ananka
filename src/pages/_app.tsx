c567242f7f67defc370c8ca19e00b9b4efc4782a
I Putu Andika Putra <andikaputra@detik.com>
2025-07-21 17:54:22 +0700

    Fix unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
-import { Inter, Playfair_Display } from 'next/font/google'
-import { Layout } from "@/components/layout/Layout";
+// import { Inter, Playfair_Display } from 'next/font/google'
+// import { Layout } from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter'
-})
+// const inter = Inter({
+//   subsets: ['latin'],
+//   variable: '--font-inter'
+// })
 
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  variable: '--font-playfair'
-})
+// const playfair = Playfair_Display({
+//   subsets: ['latin'],
+//   variable: '--font-playfair'
+// })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
